8bf27c771a5cd800b693876fa7add9c3877e9b2b
svivek <svivek@extremenetworks.com>
2020-06-14 12:04:53 -0700

    initial comment

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+
+import (
+    "context"
+    "fmt"
+    "time"
+	"tenantipmanager"
+)
+
+func main() {
+
+	ctx := context.Background()
+	tim := tenantipmanager.NewTenantIpManager (ctx, tenantipmanager.MT_IP_PREFIX)
+
+	devip := "200.1.1.2"
+
+	startTime := time.Now()
+	for j := 1; j <= 1; j++ {
+
+		tntid := fmt.Sprintf("%d", j)
+
+		for i := 1; i < 256*100; i++ {
+			devid := fmt.Sprintf("%d", i)
+			if _, err := tim.AllocateIP(ctx, tntid, devid, devip); err != nil {
+				fmt.Println(err)
+			}
+		}
+
+		//for i := 1; i < 5; i++ {
+		//	devid := fmt.Sprintf("%d", i)
+
+		//	if dev, err := tim.GetAllocatedIP(ctx, tntid, devid); err != nil {
+		//		fmt.Println(err)
+		//	} else {
+		//		tenantipmanager.PrintDevice(dev)
+		//	}
+		//}
+		fmt.Printf("Available IPs: %d\n", tim.AvailableIPs(ctx))
+		fmt.Printf("Allocated IPs: %d\n", tim.AllocatedIPs(ctx))
+	}
+	endTime := time.Now()
+	fmt.Println(endTime.Sub(startTime))
+
+	startTime = time.Now()
+	if _, err := tim.AllocateIP(ctx, "1", "256", ""); err != nil {
+
+		fmt.Println(err)
+	}
+	endTime = time.Now()
+	fmt.Println(endTime.Sub(startTime))
+
+	//for i := 1; i < 10; i++ {
+	//	devid := fmt.Sprintf("%d", i)
+	//	if _, err := tim.ReleaseIP(ctx, tntid, devid); err != nil {
+	//		fmt.Println(err)
+	//	}
+	//}
+
+	//for i := 1; i < 10; i++ {
+	//	devid := fmt.Sprintf("%d", i)
+
+	//	if dev, err := tim.GetAllocatedIP(ctx, tntid, devid); err != nil {
+	//		fmt.Println(err)
+	//	} else {
+	//		tenantipmanager.PrintDevice(dev)
+	//	}
+	//}
+}
---
+++ src/tenantipmanager/tim.go
@@ -0,0 +1,223 @@
+package tenantipmanager
+
+import (
+    "context"
+    "fmt"
+	"errors"
+    //goipam "github.com/metal-stack/go-ipam"
+	goipam "github.com/VeenaSL/go-ipam"
+)
+
+const (
+	MT_IP_PREFIX = "10.0.0.0/8"
+	//MT_IP_PREFIX = "10.10.10.0/24"
+)
+
+/*
+type DeviceIntf interface {
+
+	DeviceID() string
+	TenantID() string
+	DeviceIP() string
+	AllocatedIP() string
+}
+*/
+
+type Device struct {
+
+	tntid string	//Tenant ID
+	devid string	//Device ID
+	extip string	//External IP
+	intip *goipam.IP //Internal IP
+}
+
+func (d *Device)DeviceID() string {
+	return d.devid
+}
+
+func (d *Device)TenantID() string {
+	return d.tntid
+}
+
+func (d *Device)DeviceIP() string {
+	return d.extip
+}
+
+func (d *Device)AllocatedIP () string {
+	return d.intip.IP.String()
+}
+
+type Tenant struct {
+
+	tntid string
+	devMap map[string]*Device
+}
+
+type TenantIpManager struct {
+
+	ipam goipam.Ipamer
+	prefixVal string
+	prefix *goipam.Prefix
+	tntMap map[string]*Tenant		//Key is tntid
+}
+
+func NewTenantIpManager(ctx context.Context, prefix string) *TenantIpManager {
+
+	ipam := goipam.New()
+    ph, err := ipam.NewPrefix(prefix)
+    if err != nil {
+		fmt.Println(err)
+        return nil
+    }
+
+	ret := &TenantIpManager {
+
+		ipam: ipam,
+		prefix: ph,
+		tntMap:  make(map[string]*Tenant),
+	}
+	if prefix == "" {
+		ret.prefixVal = MT_IP_PREFIX
+	} else {
+		ret.prefixVal = prefix
+	}
+
+
+	return ret
+}
+
+//AllocateIP - Allocate IP for tenant id and device id
+func (tim *TenantIpManager)AllocateIP(ctx context.Context,
+	tntid string, devid string, devip string) (*Device, error) {
+
+	var tnt *Tenant
+	var tntOk bool
+
+	tnt, tntOk = tim.tntMap[tntid]
+	if !tntOk {
+
+		//New Tenant
+		tnt = &Tenant {
+
+			tntid: tntid,
+			devMap: make(map[string]*Device),
+		}
+
+		tim.tntMap[tntid] = tnt
+	}
+
+	var dev *Device
+	var devOk bool
+
+	dev, devOk = tnt.devMap[devid]
+	if !devOk {
+
+		//New Device
+		dev = &Device {
+
+			tntid: tntid,
+			devid: devid,
+			extip: devip,
+		}
+
+		tnt.devMap[devid] = dev
+	} else {
+
+		return dev, nil
+	}
+
+    ip, err := tim.ipam.AcquireIP(tim.prefix.Cidr)
+    if err != nil {
+        return nil, err
+    }
+
+    dev.intip = ip
+
+	return dev, nil
+}
+
+//GetAllocatedIP -
+func (tim *TenantIpManager)GetAllocatedIP(ctx context.Context,
+	tntid string, devid string) (*Device, error) {
+
+	var tnt *Tenant
+	var tntOk bool
+	tnt, tntOk = tim.tntMap[tntid]
+	if !tntOk {
+		return nil, errors.New("tenant not found")
+	}
+
+	var dev *Device
+	var devOk bool
+	dev, devOk = tnt.devMap[devid]
+	if !devOk {
+		return nil, errors.New("device not found")
+	}
+
+	return dev, nil
+}
+
+//ReleaseIP -
+func (tim *TenantIpManager)ReleaseIP(ctx context.Context,
+	tntid string, devid string) (*Device, error) {
+
+	var tnt *Tenant
+	var tntOk bool
+	tnt, tntOk = tim.tntMap[tntid]
+	if !tntOk {
+		return nil, errors.New("tenant not found")
+	}
+
+	var dev *Device
+	var devOk bool
+	dev, devOk = tnt.devMap[devid]
+	if !devOk {
+		return nil, errors.New("device not found")
+	}
+
+    _, err := tim.ipam.ReleaseIP(dev.intip)
+    if err != nil {
+        return nil, err
+    }
+	delete(tnt.devMap, devid)
+
+	if len(tnt.devMap) == 0 {
+
+		delete(tim.tntMap, tntid)
+	}
+
+	return dev, nil
+}
+
+//AllocatedIPs -
+func (tim *TenantIpManager)AllocatedIPs(ctx context.Context) uint64 {
+
+	var count uint64
+	for _, tnt := range tim.tntMap {
+
+		count += uint64(len(tnt.devMap))
+	}
+
+	return count
+}
+
+//AvailableIPs -
+func (tim *TenantIpManager)AvailableIPs(ctx context.Context) uint64 {
+
+	var count uint64
+	for _, tnt := range tim.tntMap {
+
+		count += uint64(len(tnt.devMap))
+	}
+
+	usg := tim.prefix.Usage()
+
+	return (usg.AvailableIPs - count)
+}
+
+func PrintDevice (dev *Device) {
+
+	fmt.Printf("%s %s %s %s\n", dev.TenantID(), dev.DeviceID(), dev.DeviceIP(),
+		dev.AllocatedIP())
+}
+
